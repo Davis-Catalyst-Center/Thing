27e158e2ea65646f828798d59520b433428596b2
Elizabeth Van Patten <101575298+chocoRaspberry22@users.noreply.github.com>
2024-07-19 14:08:52 -0600

    Update and rename summation_test.py to test_thing.py

--- summation_test.py
+++
@@ -1,6 +0,0 @@
-def summation(val):
-  return val + 1
-
-
-def test_summation():
-  assert summation(4) == 5
---
+++ test_thing.py
@@ -0,0 +1,7 @@
+import math.py
+
+def inc(x):
+  return x+1
+
+def test_answer():
+  assert inc(3) == 4
